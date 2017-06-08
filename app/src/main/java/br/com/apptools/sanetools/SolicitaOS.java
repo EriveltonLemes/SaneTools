de4efd4812e459db015d80ab6db460b7654a05b7
Erivelton Lemes <eriveltonlemes@hotmail.com>
2017-06-08 00:34:57 -0300

    08/06/2017 - Implementando Json *Login e registro de usuários - OK *Consulta de faturas - OK *Consulta de imóveis - OK *Consulta de ordens de serviço - implementando *Login dinâmico (cliente e equipe) -

---
+++ app/src/main/java/br/com/apptools/sanetools/SolicitaOS.java
@@ -0,0 +1,15 @@
+package br.com.apptools.sanetools;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SolicitaOS extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicita_servico);
+
+    }
+}
